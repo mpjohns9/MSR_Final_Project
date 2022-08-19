8f8f4aaac3fc32a740b29068ee5a1a68329d8f68
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-08-19 11:50:39 -0500

    Implemented network from Jupyter notebook

--- maze_gen/src/generate/cnn.py
+++ maze_gen/src/generate/cnn.py
@@ -1,13 +1,72 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv1D, AveragePooling1D, Flatten, Concatenate
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import to_categorical
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+USER = 'user4'
+NUM_CLASSES = 5
+
 
 class CNN:
 
-    # def __init__(self):
-        # self.dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-        # self.data = pd.read_csv(f'{self.dir}/data/test_results_1_revised.csv')
+    def __init__(self, num_classes=5):
+        self.net = Sequential()
+        self.net.add(Conv1D(32, (11), activation='relu', input_shape=(150,1)))
+        self.net.add(AveragePooling1D(pool_size=(5)))
+        self.net.add(Conv1D(32, (5), activation='relu'))
+        self.net.add(AveragePooling1D(pool_size=(3)))
+        self.net.add(Conv1D(32, (3), activation='relu'))
+        self.net.add(AveragePooling1D(pool_size=(3)))
 
+        self.net.add(Flatten())
+
+        self.net.add(Dense(512, activation='relu', kernel_initializer='glorot_uniform'))
+        self.net.add(Dense(100, activation='relu', kernel_initializer='glorot_uniform'))
+        self.net.add(Dense(20, activation='relu', kernel_initializer='glorot_uniform'))
+        self.net.add(Dense(num_classes, activation='softmax', kernel_initializer='glorot_uniform'))
+        
+
+    def preprocess_data(self):
+        data = pd.read_csv(f'{self.dir}/data/augmented_{USER}.csv')
+        data = data.fillna(0)
+        data = data.drop(['Unnamed: 0'], axis=1)
+
+    def process_train_test(self, data):
+        y = data['labels']
+        X = data.drop(['labels'], axis=1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.1)
+
+        X_train = np.array(X_train)
+        X_test = np.array(X_test)
+        y_train = np.array(y_train)
+        y_test = np.array(y_test)
+
+        scaler = MinMaxScaler()
+        scaler.fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
+        # Transform the target values
+        y_train_cat = to_categorical(y_train)
+        y_test_cat = to_categorical(y_test)
+
+        #Add a dimension to each sequence
+        X_train = np.array(X_train).reshape(X_train.shape[0], X_train.shape[1], 1)
+        X_test = np.array(X_test).reshape(X_test.shape[0], X_test.shape[1], 1)
+
+        return X_train, X_test, y_train, y_test, y_train_cat, y_test_cat
+
+        
     def process_data(self, data, labels=False):
         new_data = []
         for i in range(len(data) - 149):
@@ -19,6 +78,34 @@ class CNN:
 
         return new_data
 
+    def print_accuracy(self, X_test, y_test):
+        # Get the predictions and convert the multicolumn array into class predictions.
+        y_pred = np.argmax(cnn.predict(X_test), axis=1)
+
+        # Plot the confusion matrix and print the final accuracy.
+        cnn_cm = confusion_matrix(y_pred, y_test)
+        cnn_cm_disp = ConfusionMatrixDisplay(confusion_matrix=cnn_cm)
+
+        cnn_cm_disp.plot()
+        cnn_cm_disp.ax_.set_title('CNN Confusion Matrix')
+        plt.show()
+
+        print('Accuracy:', accuracy_score(y_pred, y_test))
+
+def main():
+    cnn = CNN()
+
+    data = cnn.preprocess_data()
+    X_train, X_test, y_train, y_test, y_train_cat, y_test_cat = cnn.process_train_test(data)
+
+    cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['accuracy'])
+    early = EarlyStopping(monitor='val_loss', patience=3)
+    cnn_history = cnn.fit(X_train, y_train_cat, validation_split=0.1, epochs=10, callbacks=[early])
+
+    cnn.print_accuracy(X_test, y_test)
+
+if __name__ == '__main__':
+    main()
     
 
     
