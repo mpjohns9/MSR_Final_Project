35ad25d8cecad8cd26425c4e805388922d3cd6bd
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-08-19 10:36:10 -0500

    Added documentation

--- maze_gen/src/generate/data_augmentation.py
+++ maze_gen/src/generate/data_augmentation.py
@@ -9,11 +9,13 @@ def load_data(dir):
     """Loads data to be augmented from csv file.
 
     Args:
-        dir (): _description_
+        dir (str): Path to package root directory
 
     Returns:
-        _type_: _description_
+        data (pd.DataFrame): Dataframe containing data loaded from csv
+        labels (pd.Series): Labels extracted from column of dataframe
     """
+
     file = input('Enter file name of data to be augmented:\n')
     data = pd.read_csv(f'{dir}/data/{file}')
     labels = data['labels']
@@ -21,6 +23,15 @@ def load_data(dir):
     return data, labels
 
 def center(data):
+    """Separates noise and signal and centers signal in time.
+
+    Args:
+        data (list): List of data to be centered (e.g., analog signal)
+
+    Returns:
+        centered (list): New list of same data with signal centered on noise
+    """
+
     noise = []
     signal = []
     check_noise = False
@@ -42,20 +53,38 @@ def center(data):
     return centered
 
 def compress(data):
-    # print('COMPRESS')
-    # print(data)
+    """Compresses signal and adds noise in its place.
+
+    Takes every 2-4 data points (random) and fills the rest out
+    around it with noise to reamin at length of 150.
+
+    Args:
+        data (list): List of data to be compressed 
+
+    Returns:
+        new_data (pd.Series): Compressed data
+    """
+
     factor = random.randint(2,4)
     insert_idx = round(150/factor)
     signal = data[::factor]
-    # print('Factor', factor)
     noise = [x+random.randint(-1,1) for x in np.zeros(150-insert_idx)]
     new_data = np.insert(noise, insert_idx, signal)
-    # print(len(new_data))
     return pd.Series(new_data)
 
 def expand(data):
-    # print('EXPAND')
-    # print(data)
+    """Expands signal by taking mean of consecutive data.
+
+    Uses middle 100 data points and generates new data using 
+    mean of consecutive data to maintain length of 150.
+
+    Args:
+        data (list): List of data to be expanded
+
+    Returns:
+        new_data (pd.Series): Expanded data
+    """
+
     new_data = []
     data = data[25:-25]
     for i, d in enumerate(data):
@@ -68,15 +97,21 @@ def expand(data):
         mean = np.mean([d, data[i+1]])
         new_data.extend([d, mean, data[i+1]])
 
-        
-        # print('iter:', i)
-        # print(len(new_data))
-    # print(len(new_data))
     return pd.Series(new_data)
 
 def shift_lr(data, direction):
-    # print('SHIFT ', direction.upper())
-    # print(data)
+    """Shifts signal toward front or back of list (earlier/later in time).
+
+    Args:
+        data (list): List of data to be shifted
+        direction (str): 'l' for left shift (front) and 'r' for right shift (back)
+    Raises:
+        ValueError: Raises error if 'l' or 'r' isn't given as direction
+
+    Returns:
+        new_data (pd.Series): Data shifted left (forward) or right (backward) in list
+    """
+
     new_data = []
     try:
         if direction == 'l':
@@ -89,8 +124,6 @@ def shift_lr(data, direction):
         print('Invalid direction input. String must be l or r.')
     
     for i in range(len(data)):
-        # print(direction.upper())
-        # print(i)
         if direction == 'l':
             check = i + dist <= len(data)-1
         else:
@@ -99,20 +132,24 @@ def shift_lr(data, direction):
             new_data.append(data[i+dist])
         else:
             new_data.append(random.randint(-1,1))
-    # print(len(new_data))
+
     return pd.Series(new_data)
 
 def main():
+    """Applies random transformation to existing data.
+    
+    Possible transformations include: compression, expansion, shift earlier/later in time.
+    Transformed data is saved to csv (augmented_{current_time}.csv).
+    """
+
     dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
     data, labels = load_data(dir)
     new_df = data.copy()
-    # print(data)
+
     iters = 1
     for i in range(iters):
         print(f'Iteration {i}')
-        # print(data.shape)
-        # print(new_df.shape)
         for i, row in data.iterrows():
             print(len(row))
             print(row)
@@ -124,42 +161,27 @@ def main():
             row = center(row)
             random_fn = random.randint(1, 4)
             if random_fn == 1:
-                # print('COMPRESS')
                 new_row = compress(row)
                 new_row.index = new_df.columns
                 new_row = new_row.to_frame().T
                 new_df = pd.concat([new_df, new_row])
             elif random_fn == 2:
-                # print('EXPAND')
                 new_row = expand(row)
                 new_row.index = new_df.columns
                 new_row = new_row.to_frame().T
                 new_df = pd.concat([new_df, new_row])
             elif random_fn == 3:
-                # print('SHIFT L')
                 new_row = shift_lr(row, 'l')
                 new_row.index = new_df.columns
                 new_row = new_row.to_frame().T
                 new_df = pd.concat([new_df, new_row])
             elif random_fn == 4:
-                # print('SHIFT R')
                 new_row = shift_lr(row, 'r')
                 new_row.index = new_df.columns
                 new_row = new_row.to_frame().T
                 new_df = pd.concat([new_df, new_row])
-            # print(new_df)
-        
-        # print(compressed.columns)
-        # print(expanded.columns)
-        # print(left_shift.columns)
-        # print(right_shift.columns)
-        # print(data)
-        # new_df = pd.concat([new_df, compressed, expanded, left_shift, right_shift])
-        # print(data)
     
     new_df['labels'] = labels.tolist()*(iters+1)
-    # print(new_df['labels'].value_counts())
-    # return
     new_df.to_csv(f'{dir}/data/augmented_{dt.datetime.now()}.csv')
 
 if __name__ == '__main__':
