c01a97d76c8865b17ef573b1a56216288e28bf71
Marshall Johnson <marshalljohnson2022@u.northwestern.edu>
2022-08-22 15:36:49 -0500

    Added documentation

--- maze_gen/src/generate/cnn.py
+++ maze_gen/src/generate/cnn.py
@@ -16,10 +16,16 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 USER = 'user4'
 NUM_CLASSES = 5
 
-
 class CNN:
+    """Neural network model used to classify 1D sip and puff signals."""
 
     def __init__(self, num_classes=5):
+        """Define and allocate layers.
+
+        Args:
+            num_classes (int, optional): Number of classes to predict. Defaults to 5.
+        """
+
         self.net = Sequential()
         self.net.add(Conv1D(32, (11), activation='relu', input_shape=(150,1)))
         self.net.add(AveragePooling1D(pool_size=(5)))
@@ -37,11 +43,26 @@ class CNN:
         
 
     def preprocess_data(self):
-        data = pd.read_csv(f'{self.dir}/data/augmented_{USER}.csv')
+        """Read data from csv to use in model training and testing.
+
+        Returns:
+            data (pd.DataFrame): Dataframe containing sip and puff data from csv
+        """
+
+        data = pd.read_csv(f'{ROOT_DIR}/data/augmented_{USER}.csv')
         data = data.fillna(0)
         data = data.drop(['Unnamed: 0'], axis=1)
+        return data
 
     def process_train_test(self, data):
+        """Split data into train/test sets and process.
+
+        Args:
+            data (pd.DataFrame): Dataframe containing data to be split/processed
+
+        Returns:
+            ndarray: Processed data split randomly into train and test sets
+        """
         y = data['labels']
         X = data.drop(['labels'], axis=1)
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.1)
@@ -80,7 +101,7 @@ class CNN:
 
     def print_accuracy(self, X_test, y_test):
         # Get the predictions and convert the multicolumn array into class predictions.
-        y_pred = np.argmax(cnn.predict(X_test), axis=1)
+        y_pred = np.argmax(self.net.predict(X_test), axis=1)
 
         # Plot the confusion matrix and print the final accuracy.
         cnn_cm = confusion_matrix(y_pred, y_test)
@@ -96,11 +117,12 @@ def main():
     cnn = CNN()
 
     data = cnn.preprocess_data()
+
     X_train, X_test, y_train, y_test, y_train_cat, y_test_cat = cnn.process_train_test(data)
 
-    cnn.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['accuracy'])
+    cnn.net.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['accuracy'])
     early = EarlyStopping(monitor='val_loss', patience=3)
-    cnn_history = cnn.fit(X_train, y_train_cat, validation_split=0.1, epochs=10, callbacks=[early])
+    cnn_history = cnn.net.fit(X_train, y_train_cat, validation_split=0.1, epochs=10, callbacks=[early])
 
     cnn.print_accuracy(X_test, y_test)
 
